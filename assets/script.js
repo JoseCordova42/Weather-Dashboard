46f0deb37a4e5c462af1677ff510d541c13a9a55
Jose <jose42cordova@gmail.com>
2021-01-14 17:52:48 -0600

    fixed some formatting and added some stylings

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//import dayjs from 'dayjs'
+// Dayjs------setting today's date next to city name------------
 dayjs().format();
 $("#today").text(dayjs().format('dddd, MMMM D, YYYY'));
 
@@ -8,25 +8,14 @@ var searchBtn = $("#searchBtn");
 
 // City Suggestions------------------
 var cities = $("#cityLinks");
-var austin = $("#Austin");
-var chicago = $("#Chicago");
-var newYork = $("#NewYork");
-var orlando = $("#Orlando");
-var sanFran = $("#SanFrancisco");
-var seattle = $("#Seattle");
-var denver = $("#Denver");
-var atlanta = $("#Atlanta");
 
 // Main Search Information------------------
-var cityInfo = $("#cityInfo");
 var cityDate = $("#cityPlusDate");
 var cityTemp = $("#cityTemp");
 var cityHumid = $("#cityHumid");
 var cityWind = $("#cityWind");
 
 // 5-Day Forecast------------------
-var fiveDays = $("#5Days");
-
 var day1 = $("#day1");
 var day1Date = $("#day1Date");
 var day1Temp = $("#day1Temp");
@@ -52,10 +41,11 @@ var day5Date = $("#day5Date");
 var day5Temp = $("#day5Temp");
 var day5Humid = $("#day5Humid");
 
-// Functions
+// Functions------------------
 function searchWeather() {
     var city = searchedCity.val().trim();
 
+    // Ajax call for main cityInfo display------------------
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b1a89c4bdd79dbec82ba3b66e5df9e09&units=imperial";
     $.ajax({
         url: queryURL,
@@ -71,36 +61,38 @@ function searchWeather() {
         cityWind.text("Wind Speed: " + response.wind.speed + "mph");
     });
 
+    // Ajax call for 5-day forecast------------------
     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ebe25a82700b36c9e609f29681be7620&units=imperial";
     $.ajax({
         url: fiveDayURL,
         method: "GET"        
     }).then(function(response) {
         day1Date.text(dayjs().add(1, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[3].weather[0].icon}@2x.png">`);
-        day1Temp.text("Temp: " + response.list[3].main.temp + " F");
-        day1Humid.text("Humidity: " + response.list[3].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[4].weather[0].icon}@2x.png">`);
+        day1Temp.text("Temp: " + response.list[4].main.temp + " F");
+        day1Humid.text("Humidity: " + response.list[4].main.humidity + "%");
         day2Date.text(dayjs().add(2, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[11].weather[0].icon}@2x.png">`);
-        day2Temp.text("Temp: " + response.list[11].main.temp + " F");
-        day2Humid.text("Humidity: " + response.list[11].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[12].weather[0].icon}@2x.png">`);
+        day2Temp.text("Temp: " + response.list[12].main.temp + " F");
+        day2Humid.text("Humidity: " + response.list[12].main.humidity + "%");
         day3Date.text(dayjs().add(3, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[19].weather[0].icon}@2x.png">`);
-        day3Temp.text("Temp: " + response.list[19].main.temp + " F");
-        day3Humid.text("Humidity: " + response.list[19].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[20].weather[0].icon}@2x.png">`);
+        day3Temp.text("Temp: " + response.list[20].main.temp + " F");
+        day3Humid.text("Humidity: " + response.list[20].main.humidity + "%");
         day4Date.text(dayjs().add(4, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[27].weather[0].icon}@2x.png">`);
-        day4Temp.text("Temp: " + response.list[27].main.temp + " F");
-        day4Humid.text("Humidity: " + response.list[27].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[28].weather[0].icon}@2x.png">`);
+        day4Temp.text("Temp: " + response.list[28].main.temp + " F");
+        day4Humid.text("Humidity: " + response.list[28].main.humidity + "%");
         day5Date.text(dayjs().add(5, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[35].weather[0].icon}@2x.png">`);
-        day5Temp.text("Temp: " + response.list[35].main.temp + " F");
-        day5Humid.text("Humidity: " + response.list[35].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[36].weather[0].icon}@2x.png">`);
+        day5Temp.text("Temp: " + response.list[36].main.temp + " F");
+        day5Humid.text("Humidity: " + response.list[36].main.humidity + "%");
     });
 
+    // Local Storage for searched city------------------
     localStorage.setItem("last city", JSON.stringify(city));
     var storedCity = JSON.parse(localStorage.getItem("last city"));
-
+    // Prevents blank or repeat searches------------------
     if (city === "" || cities.children().text().includes(city) === true) {
         return;
     } else {
@@ -108,19 +100,21 @@ function searchWeather() {
         <a href="#" class="list-group-item list-group-item-action" id=${storedCity}>${storedCity}</a>
     `);
     }
-
 }
 
-// Event Listeners
+// Event Listeners------------------------------------------------------
+// Search Button------------------
 searchBtn.on("click", function(event) {
     event.preventDefault();
         searchWeather();
 })
 
+// Suggested City Buttons------------------
 cities.on("click", function(event) {
     event.preventDefault();
     var city = event.target.text;
 
+    // Ajax call for main cityInfo display------------------
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b1a89c4bdd79dbec82ba3b66e5df9e09&units=imperial";
     $.ajax({
         url: queryURL,
@@ -136,30 +130,31 @@ cities.on("click", function(event) {
         cityWind.text("Wind Speed: " + response.wind.speed + "mph");
     });
 
+    // Ajax call for 5-day forecast------------------
     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ebe25a82700b36c9e609f29681be7620&units=imperial";
     $.ajax({
         url: fiveDayURL,
         method: "GET"        
     }).then(function(response) {
         day1Date.text(dayjs().add(1, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[3].weather[0].icon}@2x.png">`);
-        day1Temp.text("Temp: " + response.list[3].main.temp + " F");
-        day1Humid.text("Humidity: " + response.list[3].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[4].weather[0].icon}@2x.png">`);
+        day1Temp.text("Temp: " + response.list[4].main.temp + " F");
+        day1Humid.text("Humidity: " + response.list[4].main.humidity + "%");
         day2Date.text(dayjs().add(2, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[11].weather[0].icon}@2x.png">`);
-        day2Temp.text("Temp: " + response.list[11].main.temp + " F");
-        day2Humid.text("Humidity: " + response.list[11].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[12].weather[0].icon}@2x.png">`);
+        day2Temp.text("Temp: " + response.list[12].main.temp + " F");
+        day2Humid.text("Humidity: " + response.list[12].main.humidity + "%");
         day3Date.text(dayjs().add(3, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[19].weather[0].icon}@2x.png">`);
-        day3Temp.text("Temp: " + response.list[19].main.temp + " F");
-        day3Humid.text("Humidity: " + response.list[19].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[20].weather[0].icon}@2x.png">`);
+        day3Temp.text("Temp: " + response.list[20].main.temp + " F");
+        day3Humid.text("Humidity: " + response.list[20].main.humidity + "%");
         day4Date.text(dayjs().add(4, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[27].weather[0].icon}@2x.png">`);
-        day4Temp.text("Temp: " + response.list[27].main.temp + " F");
-        day4Humid.text("Humidity: " + response.list[27].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[28].weather[0].icon}@2x.png">`);
+        day4Temp.text("Temp: " + response.list[28].main.temp + " F");
+        day4Humid.text("Humidity: " + response.list[28].main.humidity + "%");
         day5Date.text(dayjs().add(5, 'day').format('dd - M/D/YY'))
-                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[35].weather[0].icon}@2x.png">`);
-        day5Temp.text("Temp: " + response.list[35].main.temp + " F");
-        day5Humid.text("Humidity: " + response.list[35].main.humidity + "%");
+                .append(/*html*/`<img src="https://openweathermap.org/img/wn/${response.list[36].weather[0].icon}@2x.png">`);
+        day5Temp.text("Temp: " + response.list[36].main.temp + " F");
+        day5Humid.text("Humidity: " + response.list[36].main.humidity + "%");
     });
 })
